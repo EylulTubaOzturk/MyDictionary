6c8035cbd7c58398ed556c9272701be7caaaa9d7
EylülÖztürk <eylul@DESKTOP-Q920JR7>
2021-01-23 20:13:18 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+
+        TKey[] keys;
+        TValue[] values;
+
+
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+
+
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] keyArray = keys;
+            TValue[] valueArray = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < keyArray.Length; i++)
+            {
+                for (int j = 0; j < valueArray.Length; j++)
+                {
+                    keys[i] = keyArray[i];
+                    values[j] = valueArray[j];
+                }
+
+            }
+
+
+
+        }
+
+        internal void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyDictionary;
+using System;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> isim1 = new MyDictionary<int, string>();
+            isim1.Add("Ahmet");
+            isim1.Add("Burak");
+            isim1.Add("Ayşe");
+            Console.WriteLine(isim1.Count);
+
+            MyDictionary<int, string> isim = new MyDictionary<int, string>();
+            isim.Add("Elif");
+            isim.Add("Mahmut");
+            Console.WriteLine(isim.Count);
+        }
+    }
+}
